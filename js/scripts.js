eb26d386dabc0957108fbcfe9c49741bb82bc4fd
Ivan <ivanpetrachenkov.web@gmail.com>
2025-12-08 14:08:02 +0100

    checkout page

--- js/scripts.js
+++ js/scripts.js
@@ -42,3 +42,69 @@ $(window).on('resize', () => {
 		}
 	}
 });
+
+
+$(() => {
+	//
+	$('body').on('click', '.amount__btn_minus', function (e) {
+		e.preventDefault()
+
+		let parent = $(this).closest('.amount')
+		let input = parent.find('input')
+		let inputVal = parseFloat(input.val())
+		let minimum = parseFloat(input.data('minimum'))
+		let step = parseFloat(input.data('step'))
+
+		if (inputVal > minimum) {
+			input.val(inputVal - step)
+
+			parent.find('.amount__btn_plus').prop("disabled", false)
+		}
+
+		if (inputVal-1 == minimum) {
+			$(this).prop("disabled", true)
+		}
+	})
+	
+	$('body').on('click', '.amount__btn_plus', function (e) {
+		e.preventDefault()
+
+		let parent = $(this).closest('.amount')
+		let input = parent.find('input')
+		let inputVal = parseFloat(input.val())
+		let maximum = parseFloat(input.data('maximum'))
+		let step = parseFloat(input.data('step'))
+
+		if (inputVal < maximum) {
+			input.val(inputVal + step)
+
+			parent.find('.amount__btn_minus').prop("disabled", false)
+		}
+
+		if (inputVal+1 == maximum) {
+			$(this).prop("disabled", true)
+		}
+	})
+
+	$('.amount__input').keydown(function () {
+		const _self = $(this),
+			maximum = parseInt(_self.data('maximum'))
+
+		setTimeout(() => {
+			if (_self.val() == '' || _self.val() == 0) _self.val(parseInt(_self.data('minimum')))
+			if (_self.val() > maximum) _self.val(maximum)
+		})
+	})
+
+	// Кастомный select
+	$('.select-wrap select').niceSelect()
+	$('.select-input select').niceSelect()
+
+
+	$('body').on('click', '.clear-select', function (e) {
+		e.preventDefault()
+
+		$(this).closest('.select-input').find('.list_item:eq(0)').click()
+		$(this).closest('.select-input').find('.nice-select, .current').removeClass('selected')
+	})
+});
